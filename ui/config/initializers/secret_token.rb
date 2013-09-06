0202f74fdc9d3331285858226dba6e685d7aa7f3
Howard Chung <Howard@dyn-10-181-2-118.dorm.duke.local>
2013-09-06 02:08:43 -0400

    added ui rails application

---
+++ ui/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ui::Application.config.secret_key_base = 'de4bd325adbdd0e992d21c6a5b0bc1c749227a5a8bce1ff41bbb9226ccbcd71769382c44f3ca4954874846a98eb64ecab581f1997b62c0eb99aeab5a1e00e9b9'
