253e7780dd552668ba2dddfa4f4350b4552ef71e
andyhehk <hochion@gmail.com>
2013-10-29 10:55:20 +0800

    add test

---
+++ common/src/test/scala/bigframe/bigif/XMLBigFrameInputParserTest.scala
@@ -0,0 +1,13 @@
+package bigframe.bigif
+
+import junit.framework.TestCase
+
+import java.io.InputStream
+
+class XMLBigFrameInputParserTest extends TestCase {
+
+
+	def testImportXMLFile() {
+
+	}
+}
