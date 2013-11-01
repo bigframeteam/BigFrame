f97aa14cf80ccd32b9e2483a784a887c9a0f7f5d
Seunghyun Lee <shlee0605@gmail.com>
2013-10-31 23:16:11 -0400

    fixed typo in data loader

--- common/src/main/java/bigframe/util/dataloader/vertica/VerticaTpcdsLoader.java
+++ common/src/main/java/bigframe/util/dataloader/vertica/VerticaTpcdsLoader.java
@@ -147,7 +147,7 @@ public class VerticaTpcdsLoader extends VerticaDataLoader{
 		    else if(table.equals("catalog_sales"))
 		    	 VerticaOutputFormat.setOutput(job, "catalog_sales", true, "cs_sold_date_sk int", "cs_sold_time_sk int", 
 		    			 "cs_ship_date_sk int", "cs_bill_customer_sk int", "cs_bill_cdemo_sk int", "cs_bill_hdemo_sk int",
-		    			 "cs_bill_addr_sk, int", "cs_ship_customer_sk int", "cs_ship_cdemo_sk int", "cs_ship_hdemo_sk int",
+		    			 "cs_bill_addr_sk int", "cs_ship_customer_sk int", "cs_ship_cdemo_sk int", "cs_ship_hdemo_sk int",
 		    			 "cs_ship_addr_sk int", "cs_call_center_sk int", "cs_catalog_page_sk int", "cs_ship_mode_sk int",
 		    			 "cs_warehouse_sk int", "cs_item_sk int", "cs_promo_sk int", "cs_order_number int", "cs_quantity int",
 		    			 "cs_wholesale_cost float", "cs_list_price float", "cs_sales_price float", "cs_ext_discount_amt float",
