8a4501a66ad478ef56384713ff2e2cff0e838b13
Andy He <hochion@gmail.com>
2013-11-10 23:28:03 +0800

    Create spark-env.sh

---
+++ conf/spark-env.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+######################### SPARK RELATED ##########################
+
+# Path of Spark installation home. 
+export SPARK_HOME="/home/shlee0605/spark-0.8.0"
+
+# Path of Scala installation home.
+export SCALA_HOME="/home/shlee0605/scala-2.9.3"
+
+# Spark memory parameters, defaults will be used if unspecified
+export SPARK_MEM=2g
+# export SPARK_WORKER_MEMORY=6g
+
+# Spark connection string, available in Spark master's webUI
+export SPARK_CONNECTION_STRING="spark://ubuntu:7077"
+
+# The Spark Home Directory
+SPARK_HOME=$SPARK_HOME
+
+# The Shark Home
+SHARK_HOME=$SHARK_HOME
+BIGFRAME_OPTS="${BIGFRAME_OPTS} -Dbigframe.shark.home=${SHARK_HOME}"
+
+# The Spark Master Address
+SPARK_MASTER=$SPARK_CONNECTION_STRING
+BIGFRAME_OPTS="${BIGFRAME_OPTS} -Dbigframe.spark.master=${SPARK_MASTER}"
